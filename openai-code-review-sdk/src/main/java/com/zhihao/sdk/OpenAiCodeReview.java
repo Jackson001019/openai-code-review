a6c4cd55154f035de0552fe9169a0b5c45b6cff5
Jackson <1249184132@qq.com>
2025-06-19 15:20:47 +0800

    feat:检出代码

--- openai-code-review-sdk/src/main/java/com/zhihao/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/zhihao/sdk/OpenAiCodeReview.java
@@ -1,5 +1,7 @@
 package com.zhihao.sdk;
 
+import java.io.*;
+
 /**
  * @author Jackson
  * @create 2025/6/19 10:39
@@ -7,8 +9,26 @@ package com.zhihao.sdk;
  */
 public class OpenAiCodeReview {
 
-    public static void main(String[] args) {
-        System.out.println("OpenAiCodeReview");
+    public static void main(String[] args) throws Exception {
+        System.out.println("OpenAiCodeReview Test Execute");
+
+        // 1. 检出github最近2次代码记录
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code " + exitCode);
+
+        System.out.println("review code："+ diffCode.toString());
     }
 
 }
